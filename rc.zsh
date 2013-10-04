39c41ab526a07b5a523fd8ddd3d20c99e53d1146
Leo Adamek <leo@adamek.me>
2013-10-04 11:04:37 +0100

    Added support for programs in /opt

--- rc.zsh
+++ rc.zsh
@@ -1,3 +1,6 @@
+# ZSH OPTIONS
+setopt extendedglob
+
 # Path to your oh-my-zsh configuration.
 ZSH=$HOME/.oh-my-zsh
 
@@ -7,41 +10,20 @@ ZSH=$HOME/.oh-my-zsh
 # time that oh-my-zsh is loaded.
 ZSH_THEME="dieter"
 
-# Example aliases
-# alias zshconfig="mate ~/.zshrc"
-# alias ohmyzsh="mate ~/.oh-my-zsh"
+plugins=(git github colorize nyan lol archlinux themes web-search svn rsync)
 
-# Set to this to use case-sensitive completion
-# CASE_SENSITIVE="true"
-
-# Comment this out to disable bi-weekly auto-update checks
-# DISABLE_AUTO_UPDATE="true"
-
-# Uncomment to change how often before auto-updates occur? (in days)
-# export UPDATE_ZSH_DAYS=13
-
-# Uncomment following line if you want to disable colors in ls
-# DISABLE_LS_COLORS="true"
-
-# Uncomment following line if you want to disable autosetting terminal title.
-# DISABLE_AUTO_TITLE="true"
-
-# Uncomment following line if you want to disable command autocorrection
-# DISABLE_CORRECTION="true"
-
-# Uncomment following line if you want red dots to be displayed while waiting for completion
-# COMPLETION_WAITING_DOTS="true"
-
-# Which plugins would you like to load? (plugins can be found in ~/.oh-my-zsh/plugins/*)
-# Custom plugins may be added to ~/.oh-my-zsh/custom/plugins/
-# Example format: plugins=(rails git textmate ruby lighthouse)
-plugins=(git)
 
 source $ZSH/oh-my-zsh.sh
 
-# Customize to your needs...
 export PATH=$PATH:/usr/local/sbin:/usr/local/bin:/usr/bin:/usr/bin/core_perl
-export PATH=$PATH:~/.bin
 
 # Change $TERM to 'xterm' so we can ssh properly
 alias ssh="export TERM='xterm';ssh"
+
+# Alias for dev mysql
+alias dev-mysql="mysql -h 192.168.56.102 -udev -p"
+
+##
+# Additional Paths
+##
+[[ -d '/opt/php/bin' ]] && export PATH=$PATH:/opt/php/bin
