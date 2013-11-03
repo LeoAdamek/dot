c4dc9e8cb69bbde67f97f558dcd4d6ad43579035
Leo Adamek <leo@adamek.me>
2013-11-03 22:22:23 +0000

    Modified rc.zsh & added ghci config

--- rc.zsh
+++ rc.zsh
@@ -4,27 +4,49 @@ setopt extendedglob
 # Path to your oh-my-zsh configuration.
 ZSH=$HOME/.oh-my-zsh
 
+#--------------------------------------------------------------------------------
 # Set name of the theme to load.
 # Look in ~/.oh-my-zsh/themes/
 # Optionally, if you set this to "random", it'll load a random theme each
 # time that oh-my-zsh is loaded.
+#--------------------------------------------------------------------------------
 ZSH_THEME="dieter"
 
+#--------------------------------------------------------------------------------
+# Plugins
+#--------------------------------------------------------------------------------
 plugins=(git github colorize nyan lol archlinux themes web-search svn rsync)
 
-
+# Load Oh-my-zsh
 source $ZSH/oh-my-zsh.sh
 
-export PATH=$PATH:/usr/local/sbin:/usr/local/bin:/usr/bin:/usr/bin/core_perl
+#--------------------------------------------------------------------------------
+# Update PATH & EDITOR
+#--------------------------------------------------------------------------------
+export PATH=$PATH:/usr/local/sbin:/usr/local/bin:/usr/bin
+export EDITOR='emacs -nw'
 
-# Change $TERM to 'xterm' so we can ssh properly
+
+#--------------------------------------------------------------------------------
+# Set Pager to `most` if it is available.
+#--------------------------------------------------------------------------------
+command -v most &> /dev/null && export PAGER=most
+
+#--------------------------------------------------------------------------------
+# Change $TERM to 'xterm'
+# When using SSH
+# So we can use systems which don't understand a 256colour terminal
+#--------------------------------------------------------------------------------
 alias ssh="export TERM='xterm';ssh"
 
-# Alias for dev mysql
-alias dev-mysql="mysql -h 192.168.56.102 -udev -p"
+#--------------------------------------------------------------------------------
+# RVM Settings + Initialization
+#--------------------------------------------------------------------------------
+source ~/.rvm/scripts/rvm
 
-##
+#--------------------------------------------------------------------------------
 # Additional Paths
-##
-[[ -d '/opt/php/bin' ]] && export PATH=$PATH:/opt/php/bin
-[[ -d '/opt/mongodb/bin' ]] && export PATH=$PATH:/opt/mongodb/bin
+#--------------------------------------------------------------------------------
+[[ -d '/opt/php/bin'      ]] && export PATH=$PATH:/opt/php/bin
+[[ -d '/opt/mongodb/bin'  ]] && export PATH=$PATH:/opt/mongodb/bin
+[[ -d '/opt/postgres/bin' ]] && export PATH=$PATH:/opt/postgres/bin
