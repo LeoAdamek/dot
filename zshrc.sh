ea64d450476a297683d130ed66ef484a045484f4
Leo Adamek <leo@adamek.me>
2014-09-21 21:57:50 +0100

    Refactoring of ZSHRC and SSH-AGENT things

--- zshrc.sh
+++ zshrc.sh
@@ -28,16 +28,18 @@ plugins=(git github colorize nyan lol archlinux cp theme catimg npm bundler syst
 source $ZSH/oh-my-zsh.sh
 
 #--------------------------------------------------------------------------------
-# SSH Agent
+# SSH Agent -- Only if one isn't already open (TODO!)
 #--------------------------------------------------------------------------------
-# eval $(ssh-agent) &> /dev/null
-
-# Private keys all end in `.pem`
-# (Required to filter out publics and configs)
-#ssh-add $HOME/.ssh/*.pem &> /dev/null
+if [[ $SSH_AGENT_PID == 0 ]]; then
+    eval $(ssh-agent) &> /dev/null
+    
+    # Private keys all end in `.pem`
+    # (Required to filter out publics and configs)
+    ssh-add $HOME/.ssh/*.pem &> /dev/null
+fi
 
 #--------------------------------------------------------------------------------
-# Set EDITOR to `emacs -nw`
+# Set EDITOR to `emacs -nw` if emacs is installed
 #--------------------------------------------------------------------------------
 which emacs &> /dev/null \
     && export EDITOR="emacs -nw"
@@ -47,14 +49,6 @@ which emacs &> /dev/null \
 # As remote hosts won't be configued for a 256-colour temrinal.
 #--------------------------------------------------------------------------------
 alias ssh="TERM=xterm ssh"
-
-#--------------------------------------------------------------------------------
-# (other) Command Aliases
-#--------------------------------------------------------------------------------
-# Always color pacman output
-alias pacman="pacman --color=always"
-# Amazing Git fancy-log
-alias git-fancy-log="git log --graph --abbrev-commit --decorate --date=relative --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(bold yellow)%d%C(reset)' --all"
 
 
 #--------------------------------------------------------------------------------
@@ -66,24 +60,38 @@ alias git-fancy-log="git log --graph --abbrev-commit --decorate --date=relative 
 # Additional Paths
 # Will check for each of the following directories and append it to the path.
 #--------------------------------------------------------------------------------
+# PHP from source
 [[ -d '/opt/php/bin'      ]] && export PATH=$PATH:/opt/php/bin
 
+## Databases etc
 [[ -d '/opt/mongodb/bin'  ]] && export PATH=$PATH:/opt/mongodb/bin
 [[ -d '/opt/postgres/bin' ]] && export PATH=$PATH:/opt/postgres/bin
 [[ -d '/opt/redis/bin'    ]] && export PATH=$PATH:/opt/redis/bin
 
 [[ -d '/opt/arduino'      ]] && export PATH=$PATH:/opt/arduino
 
+## 3D Printing
 [[ -d '/opt/pronterface'  ]] && export PATH=$PATH:/opt/printerface
 [[ -d '/opt/slic3r'       ]] && export PATH=$PATH:/opt/slic3r
 [[ -d '/opt/idea'         ]] && export PATH=$PATH:/opt/idea/bin
 
+## RVM
 [[ -d "$HOME/.bin"        ]] && export PATH=$PATH:$HOME/.bin
 [[ -d "$HOME/.rvm/bin"    ]] && export PATH=$PATH:$HOME/.rvm/bin
 
 #--------------------------------------------------------------------------------
 # Aliases
 #--------------------------------------------------------------------------------
+
+# Shibe git
 alias vim='emacs -nw'
 alias wow='git status'
 alias commit='git commit -m'
+
+# Always color pacman output
+alias pacman="pacman --color=always"
+# Amazing Git fancy-log
+alias git-fancy-log="git log --graph --abbrev-commit --decorate --date=relative --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(bold yellow)%d%C(reset)' --all"
+
+# Dalek Delete
+alias exterminate="rm -rf"
